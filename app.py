534594c709a0ad0d185542eb1c2c0becbc83b21d
Christian Zigah <efochristian@outlook.com>
2019-10-08 15:20:58 +0100

    Edit index.html

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-class Todo(db.Model):
+class Todo(db.Model):        
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     # completed = db.Column(db.Interger, default=0)
@@ -20,12 +21,13 @@ class Todo(db.Model):
     def __repr__(self):
         return '<Task %r>' % self.id
 
+  
 
-
-@app.route("/")
+@app.route("/", methods=['POST', 'GET'])  
 def index():
     return render_template("index.html")
 
 
 if __name__ == "__main__":
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     app.run(debug=True) 
