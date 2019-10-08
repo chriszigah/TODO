d8b73747ea6e94c9a0d0a266bfd8b7447abb33ff
Christian Zigah <efochristian@outlook.com>
2019-10-08 17:18:19 +0100

    updates

--- app.py
+++ app.py
@@ -49,7 +49,21 @@ def delete(id):
 
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
-    return ' '
+    
+    task = Todo.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue updating you task.'
+            
+
+    else:
+        return render_template('update.html', task=task)
 
 
 if __name__ == "__main__":
