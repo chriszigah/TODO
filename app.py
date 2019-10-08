5d8efd7574441e5d1a43184aa6d8bd894a4cc1a0
Christian Zigah <efochristian@outlook.com>
2019-10-08 14:29:46 +0100

    Initialize Sqlite3 DB

--- app.py
+++ app.py
@@ -1,4 +1,25 @@
 from flask import Flask, render_template, url_for
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    # completed = db.Column(db.Interger, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __init__(self, **kwargs):
+        super(self).__init__(**kwargs)
+
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
 
 
 @app.route("/")
